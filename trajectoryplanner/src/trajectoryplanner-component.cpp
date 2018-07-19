335cf7002ec370e8387c48c5dd1b45c32241f8a3
TiderFang <34900738+TiderFang@users.noreply.github.com>
2018-07-19 22:02:48 +0800

    First commit
    this is the part of trajectory planner!

---
+++ trajectoryplanner/src/trajectoryplanner-component.cpp
@@ -0,0 +1,60 @@
+#include "trajectoryplanner-component.hpp"
+#include <rtt/Component.hpp>
+#include <iostream>
+
+Trajectoryplanner::Trajectoryplanner(std::string const& name) : TaskContext(name){
+  std::cout << "Trajectoryplanner constructed !" <<std::endl;
+}
+
+bool Trajectoryplanner::configureHook(){
+  std::cout << "Trajectoryplanner configured !" <<std::endl;
+  return true;
+}
+
+bool Trajectoryplanner::startHook(){
+  std::cout << "Trajectoryplanner started !" <<std::endl;
+  return true;
+}
+
+void Trajectoryplanner::updateHook(){
+  std::cout << "Trajectoryplanner executes updateHook !" <<std::endl;
+}
+
+void Trajectoryplanner::stopHook() {
+  std::cout << "Trajectoryplanner executes stopping !" <<std::endl;
+}
+
+void Trajectoryplanner::cleanupHook() {
+  std::cout << "Trajectoryplanner cleaning up !" <<std::endl;
+}
+
+//define the operation function
+bool Trajectoryplanner::moveLine(Frame start, Frame end){
+	try{
+		//step1:create path
+	    _path = new Path_Line(start ,end ,new RotationalInterpolation_SingleAxis(), _eqradio, True);
+	    //step2:create velocity profile
+	    _velocityprofile = new VelocityProfile_Trap(_velocityprofile_maxvel, _velocityprofile_maxacc);
+	    _velocityprofile->SetProfile(0, path->PathLength());
+	    trajectory = new Trajectory_Segment(_path, _velocityprofile);
+	    return true;
+	}
+    catch(Error & error){
+  	    std::cout <<"I encountered this error : " << error.Description() << std::endl;
+	    std::cout << "with the following type " << error.GetType() << std::endl;
+	    return false;
+  	}
+}
+/*
+ * Using this macro, only one component may live
+ * in one library *and* you may *not* link this library
+ * with another component library. Use
+ * ORO_CREATE_COMPONENT_TYPE()
+ * ORO_LIST_COMPONENT_TYPE(Trajectoryplanner)
+ * In case you want to link with another library that
+ * already contains components.
+ *
+ * If you have put your component class
+ * in a namespace, don't forget to add it here too:
+ */
+ORO_CREATE_COMPONENT(Trajectoryplanner)
---
+++ trajectoryplanner/src/trajectoryplanner-component.hpp
@@ -0,0 +1,55 @@
+#ifndef OROCOS_TRAJECTORYPLANNER_COMPONENT_HPP
+#define OROCOS_TRAJECTORYPLANNER_COMPONENT_HPP
+
+#include <rtt/RTT.hpp>
+#include <rtt/Property.hpp>
+#include <rtt/Attribute.hpp>
+#include <rtt/Operation.hpp>
+#include <rtt/Port.hpp>
+
+//trajectory realted 
+#include <frames.hpp>
+#include <frames_io.hpp>
+#include <trajectory.hpp>
+#include <trajectory_segment.hpp>
+#include <trajectory_stationary.hpp>
+#include <trajectory_composite.hpp>
+#include <trajectory_composite.hpp>
+#include <velocityprofile_trap.hpp>
+#include <path_roundedcomposite.hpp>
+#include <rotational_interpolation_sa.hpp>
+#include <utilities/error.h>
+#include <trajectory_composite.hpp>
+
+using namespace KDL;
+using namespace RTT;
+//component setting
+class Trajectoryplanner : public RTT::TaskContext{
+  public:
+    Trajectoryplanner(std::string const& name);
+    bool configureHook();
+    bool startHook();
+    void updateHook();
+    void stopHook();
+    void cleanupHook();
+  public:
+  // trajectory operation
+    bool moveLine(Frame start,Frame end);
+    bool moveCircle();
+    bool moveCircle_close();
+    bool movePTP();
+  public:
+  // property and attribute for velocity profile
+    double _velocityprofile_maxacc;
+    double _velocityprofile_maxvel;
+  public:
+  // property for path
+    double _eqradio;
+    double _scale;
+  public:
+  //trajectory key variables
+    Trajectory * _trajectory;
+    VelocityProfile * _velocityprofile;
+    Path * path;
+};
+#endif
